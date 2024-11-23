5fa4733ff68e3fd0b3934ac1ca43a69cde6fbbfc
karim <karim.itani@hec.ca>
2024-11-23 15:42:46 -0500

    moved the shift function into the utils.c file

--- DSA_in_C/utils/utils.c
+++ DSA_in_C/utils/utils.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-
-
 void printArray(int arr[], int size)
 {
     for (int i = 0; i < size; i++) {
@@ -17,17 +15,17 @@ void swap(int array_to_sort[] , int pos1, int pos2)
     array_to_sort[pos2] = temp;
 }
 
-// void shift(int arr[], int shift_end_idx, int shift_start_idx)
-// {
-//     if (shift_start_idx >= shift_end_idx )
-//     {
-//         return;
-//     }
+void shift(int arr[], int shift_end_idx, int shift_start_idx)
+{
+    if (shift_start_idx >= shift_end_idx )
+    {
+        return;
+    }
 
-//     int temp = arr[shift_end_idx];
-//     for (int k = 0; k <= shift_end_idx - shift_start_idx - 1; k++)
-//     {
-//         arr[shift_end_idx - k] = arr[shift_end_idx - k - 1];
-//     }
-//     arr[shift_start_idx] = temp;
-// }
+    int temp = arr[shift_end_idx];
+    for (int k = 0; k <= shift_end_idx - shift_start_idx - 1; k++)
+    {
+        arr[shift_end_idx - k] = arr[shift_end_idx - k - 1];
+    }
+    arr[shift_start_idx] = temp;
+}
--- DSA_in_C/utils/utils.h
+++ DSA_in_C/utils/utils.h
@@ -6,4 +6,4 @@ void printArray(int arr[], int size);
 
 void swap(int array_to_sort[], int pos1, int pos2);
 
-// void shift(int arr[], int shift_end_idx, int shift_start_idx);
+void shift(int arr[], int shift_end_idx, int shift_start_idx);
